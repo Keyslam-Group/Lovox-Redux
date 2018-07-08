57b7a16d79448dea14c7e67c5465c2933b6d23c8
Justin van der Leij <justinvanderleij@gmail.com>
2018-07-08 14:47:30 +0200

    Added depth testing + Z axis

--- main.lua
+++ main.lua
@@ -11,12 +11,12 @@ local sqrt, atan2 = math.sqrt, math.atan2
 local cos,  sin   = math.cos,  math.sin
 
 
-local voxelCount  = 399
+local voxelCount  = 800
 local vertexCount = voxelCount * 6 * 16
 
 FFI.cdef[[
    typedef struct {
-      float x, y;
+      float x, y, z;
       float u, v;
    } fm_vertex;
 ]]
@@ -28,7 +28,7 @@ local byteData  = love.data.newByteData(memoryUsage)
 local voxelData = FFI.cast("fm_vertex*", byteData:getPointer())
 
 local Voxel_mesh = love.graphics.newMesh({
-   {"VertexPosition", "float", 2},
+   {"VertexPosition", "float", 3},
    {"VertexTexCoord", "float", 2},
 }, byteData, "triangles", "dynamic")
 Voxel_mesh:setTexture(Image)
@@ -40,12 +40,13 @@ local step = 1 / 16
 local rectDiag  = sqrt (halfWidth * halfWidth + halfHeight * halfHeight)
 local rectAngle = atan2(halfHeight, halfWidth)
 
-local function writeVertex(id, x, y, u, v)
+local function writeVertex(id, x, y, z, u, v)
    local vt = voxelData[id]
-   vt.x, vt.y, vt.u, vt.v = x, y, u, v
+   vt.x, vt.y, vt.z = x, y, z / 32
+   vt.u, vt.v       = u, v
 end
 
-function updateVoxel(id, x, y, rotation)
+function updateVoxel(id, x, y, z, rotation)
    local offset = id * 96
 
    local rot, iRot = rectAngle + rotation, -rectAngle + rotation
@@ -62,26 +63,47 @@ function updateVoxel(id, x, y, rotation)
       local start_u, end_u = layer * step, layer * step + step
       local o = offset + (layer * 6)
 
-      writeVertex(o + 0, tl_x, tl_y - layer, start_u, 0)
-      writeVertex(o + 1, tr_x, tr_y - layer, end_u,   0)
-      writeVertex(o + 2, bl_x, bl_y - layer, start_u, 1)
+      writeVertex(o + 0, tl_x, tl_y, z + layer, start_u, 0)
+      writeVertex(o + 1, tr_x, tr_y, z + layer, end_u,   0)
+      writeVertex(o + 2, bl_x, bl_y, z + layer, start_u, 1)
 
-      writeVertex(o + 3, br_x, br_y - layer, end_u,   1)
-      writeVertex(o + 4, bl_x, bl_y - layer, start_u, 1)
-      writeVertex(o + 5, tr_x, tr_y - layer, end_u,   0)
+      writeVertex(o + 3, br_x, br_y, z + layer, end_u,   1)
+      writeVertex(o + 4, bl_x, bl_y, z + layer, start_u, 1)
+      writeVertex(o + 5, tr_x, tr_y, z + layer, end_u,   0)
    end
 end
 
+local color = love.graphics.newCanvas(w, h, {format = "rgba8"})
+local depth = love.graphics.newCanvas(w, h, {format = "depth24"})
+
+local canvas = {color, depthstencil = depth}
+love.graphics.setDepthMode("lequal", true)
+
 local voxels = {}
+
+local player = {
+   id = #voxels,
+   x = 20,
+   y = 20,
+   z = 0,
+   r = 0,
+
+   velocity = 0,
+   speed    = 50,
+
+   rotVelocity = 0,
+   rotSpeed    = 2,
+}
+
 function love.load()
-   for x = 0, 1280, 64 do
-      for y = 0, 720, 40 do
+   for x = 0, 1280, 128 do
+      for y = 0, 720, 60 do
          local voxel = {
-            x = x, y = y,
-            r = love.math.random() * math.pi * 2
+            x = x, y = y, z = 0,
+            r = 0.2
          }
       
-         updateVoxel(#voxels, voxel.x, voxel.y, voxel.r)
+         updateVoxel(#voxels, voxel.x, voxel.y, voxel.z, voxel.r)
          Voxel_mesh:setVertices(byteData)
       
          table.insert(voxels, voxel)
@@ -91,32 +113,66 @@ function love.load()
    Voxel_mesh:setVertices(byteData)
 end
 
+local r = 0
 function love.update(dt)
    love.window.setTitle(love.timer.getFPS())
 
    for i, voxel in ipairs(voxels) do
       voxel.r = voxel.r + dt
-      updateVoxel(i - 1, voxel.x, voxel.y, voxel.r)
+      --updateVoxel(i - 1, voxel.x, voxel.y, voxel.z, voxel.r)
    end
 
+   if love.keyboard.isDown("w") then player.velocity = player.velocity + player.speed * dt end
+   if love.keyboard.isDown("s") then player.velocity = player.velocity - player.speed * dt end
+
+   if love.keyboard.isDown("q") then player.rotVelocity = player.rotVelocity - player.rotSpeed * dt end
+   if love.keyboard.isDown("e") then player.rotVelocity = player.rotVelocity + player.rotSpeed * dt end
+
+   if love.keyboard.isDown("r") then player.z = player.z + dt * 10 end
+   if love.keyboard.isDown("f") then player.z = player.z - dt * 10 end
+
+   player.r = player.r + player.rotVelocity * dt
+
+   player.x = player.x + player.velocity * cos(player.r) * dt
+   player.y = player.y + player.velocity * sin(player.r) * dt
+
+   player.rotVelocity = player.rotVelocity - (player.rotVelocity * 0.8 * dt)
+
+   updateVoxel(player.id, player.x, player.y, player.z, player.r)
+
    Voxel_mesh:setVertices(byteData)
+
+   r = r + dt / 4
 end
 
 function love.draw()
-   love.graphics.scale(1)
-   --love.graphics.setShader(Shader)
-      love.graphics.draw(Voxel_mesh)
-   --love.graphics.setShader()
+   
+   love.graphics.setCanvas(canvas)
+      love.graphics.clear(0, 0, 0, 1, true, 1)
+
+      love.graphics.push()
+      love.graphics.translate(_WIDTH/2, _HEIGHT/2)
+      love.graphics.rotate(r)
+
+      
+      love.graphics.setShader(Shader)
+         love.graphics.draw(Voxel_mesh)
+      love.graphics.setShader()
+
+      love.graphics.pop()
+   love.graphics.setCanvas()
+
+   love.graphics.draw(canvas[1])
 end
 
 local c = 0
 function love.mousepressed(x, y, btn)
    local voxel = {
-      x = x, y = y,
-      r = love.math.random() * math.pi * 2
+      x = x, y = y, z = 0,
+      r = love.math.random() * math.pi * 2,
    }
 
-   updateVoxel(#voxels, voxel.x, voxel.y, voxel.r)
+   updateVoxel(#voxels, voxel.x, voxel.y, voxel.z, voxel.r)
    Voxel_mesh:setVertices(byteData)
 
    table.insert(voxels, voxel)
--- shader.glsl
+++ shader.glsl
@@ -1,18 +1,26 @@
 #pragma language glsl3
 
 #ifdef VERTEX
+ //projection * vec4(x, y, z, 1) = vec4(x, y-z, y, 1)
+const mat4 projection = mat4(
+    1,  0, 0, 0,
+    0,  1, -1, 0,
+    0,  1, 0, 0,
+    0,  0, 0, 1
+);
 
-vec4 position(mat4 transform_projection, vec4 vertex_position) {
-   return transform_projection * vertex_position;
+vec4 position(mat4 ortho, vec4 vertex) {
+    return projection * ortho * vertex;
 }
-
 #endif
 
 #ifdef PIXEL
-
 vec4 effect(vec4 color, sampler2D img, vec2 texture_coords, vec2 screen_coords) {
     vec4 pixel = Texel(img, texture_coords);
-    return pixel * color;
-} 
+    
+    if (pixel.a == 0.0)
+        discard;
 
+    return pixel * color;
+}
 #endif
